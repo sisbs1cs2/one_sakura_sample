8996201181fa8060c972df4330cc18c8fd89b287
shotarosdk <>
2020-03-09 19:01:53 +0900

    テストエラーケース

--- test/CalcurateModel.js
+++ test/CalcurateModel.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert;
 const target = require('../src/models/CalcurateModel');
 
 describe('CalcurateModelのテスト', () => {
-    it('消費税計算１', () => {
+    it('消費税計算８％', () => {
         let objTarget = new target();
         let price = 100;
         let actual = objTarget.calcurateTax(price);
